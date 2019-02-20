1eeada72df0237ee34fdff43d6d03364d26ca4e4
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-20 15:52:03 -0800

    API showing on page

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Character({ character }) {
-  const { name, status, species, gender } = character;
+  const { name, status, species, gender, image } = character;
   return (
     <>
       <h2>{name}</h2>
       <h4>{status}</h4>
       <h4>{species}</h4>
       <h4>{gender}</h4>
+      <img src={image}/>
     </>
   );
 }
