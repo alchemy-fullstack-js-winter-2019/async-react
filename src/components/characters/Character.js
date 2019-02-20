e2ff71e55e9e9c3d6895f6a2284ecb758b1f2b44
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-20 14:54:01 -0800

    App test passing

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Character({ character }) {
-  
+  const { name, status, species, gender } = character;
   return (
     <>
-      <h2>{character.name}</h2>
-      <h4>{character.status}</h4>
-      <h4>{character.species}</h4>
-      <h4>{character.gender}</h4>
+      <h2>{name}</h2>
+      <h4>{status}</h4>
+      <h4>{species}</h4>
+      <h4>{gender}</h4>
     </>
   );
 }
