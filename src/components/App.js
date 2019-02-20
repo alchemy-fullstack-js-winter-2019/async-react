b92d543c24ccdfbfefd861b987269789422cc578
Kate Dameron <katerj33@gmail.com>
2019-02-20 14:36:43 -0800

    updated babelrc and package.json plust first snapshot test for App

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,6 @@ import React, { Component } from 'react';
 export default class App extends Component {
 
   render() {
-    return <h1>My List of things</h1>
+    return <h1>My List of things</h1>;
   }
 }
