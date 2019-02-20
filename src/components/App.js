86d0540e8a5d43dfd76edfaea98ffaafaf22989e
Carmen Perezchica <crperezchica@gmail.com>
2019-02-20 15:35:24 -0800

    characters and test passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+// import Characters from '../components/characters/Characters';
 
 export default class App extends Component {
   render() {
     return <h1>My List of Things</h1>;
+    // <Characters />;
   }
 }
