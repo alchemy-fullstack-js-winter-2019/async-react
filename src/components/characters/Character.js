c9d308a300ab5f99a2ef4524bac9886669e83a8d
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-20 14:53:38 -0800

    character snapshot works

---
+++ src/components/characters/Character.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Character extends PureComponent {
+    static propTypes = {
+      character: PropTypes.object.isRequired
+    }
+    render() {
+      const { character } = this.props;
+      return (
+        <li>
+          <h3>{character.name}</h3>
+          <h4>{character.species}</h4>
+          <h5>{character.status}</h5>
+          <h5>{character.gender}</h5>
+        </li>
+      );
+    }
+}
+
+
