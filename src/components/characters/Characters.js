62e377d4f7a82cfa715b4e77b029875db2c2d60b
Shaba9 <shaba92@gmail.com>
2019-02-20 15:25:46 -0800

    characters changed to a state; passes

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,14 +1,34 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import Character from './Character';
 
+const results = [
+  {
+    name: 'bob',
+    img: 'https://www.littledayout.com/wp-content/uploads/articles/2017/07-July/20170715-WorldEmojiDay/DabEmoji.jpg',
+    status: 'alive',
+    species: 'human',
+    gender: 'mail'
+  },
+  {
+    name: 'bob2',
+    img: 'https://www.littledayout.com/wp-content/uploads/articles/2017/07-July/20170715-WorldEmojiDay/DabEmoji.jpg',
+    status: 'dead',
+    species: 'animal',
+    gender: 'mail' 
+  }
+];
+
 export default class Characters extends PureComponent {
-  static propTypes = {
-    characters: PropTypes.array.isRequired
+  state = {
+    characters: []
+  }
+
+  componentDidMount() {
+    this.setState({ characters: results });
   }
 
   render() {
-    const charactersList = this.props.characters.map(char => {
+    const charactersList = this.state.characters.map(char => {
       return <Character key={char.name}
         character={char}
       />;
