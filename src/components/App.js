63c38ba8e33b74a3dbd9170d967e8d7c906f3cf6
Paige Gorry <paigeegorry@gmail.com>
2019-02-20 10:28:02 -0800

    basic set-up for lab

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent, Fragment } from 'react';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <Fragment>
+        <h1>Async React</h1>
+        <p>async react</p>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
