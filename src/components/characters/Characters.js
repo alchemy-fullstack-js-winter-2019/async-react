2fc305aba01fd646e232e415f67eea3ee0c25bb1
Kate Dameron <katerj33@gmail.com>
2019-02-21 12:29:12 -0800

    updated Characters component to be exported with paging

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,25 +1,54 @@
 import React, { PureComponent } from 'react';
 import Character from './Character';
 import { getCharacters } from '../../services/rickAndMortyApi';
+import styles from '../css/Characters.css';
+import PropTypes from 'prop-types';
+import WithPaging from '../Paging';
 
-export default class Characters extends PureComponent {
+export class Characters extends PureComponent {
+
+  static propTypes = {
+    page: PropTypes.number,
+    updateTotalPages: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    page: 1,
+
+  }
+
   state = {
     characters: []
   };
 
-  componentDidMount() {
-    getCharacters()
+  // Call the updateTotalPages function (that was passed as a prop) to notify App what total pages should be inside this fetch function
+  fetchCharacters = () => {
+    getCharacters(this.props.page)
       .then(res => {
+        this.props.updateTotalPages(res.totalPages);
         this.setState({ characters: res.results });
       });
+  }
+
+  // initial Api call for characters to load page
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  // only update (call Api again) if the prevProps aren't available
+  componentDidUpdate(prevProps) {
+    if(prevProps !== this.props.page) {
+      this.fetchCharacters();
+    }
   }
 
   render() {
     const listOfChars = this.state.characters.map(char => {
       return (
         <Character 
-          key={char.name}
+          key={char.id}
           name={char.name}
+          image={char.image}
           status={char.status}
           species={char.species}
           gender={char.gender}
@@ -28,9 +57,11 @@ export default class Characters extends PureComponent {
     });
 
     return (
-      <table>
+      <div className={styles.page}>
         {listOfChars}
-      </table>
+      </div>
     );
   }
 }
+
+export const CharactersWithPaging = WithPaging(Characters);
