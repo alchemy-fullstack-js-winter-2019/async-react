ef0f4c606524884174e71b53c88b07dd3b5ec7c4
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-20 16:17:24 -0800

    Iinitial commit; App and Character snapshots tests;

---
+++ src/components/characters/Characters.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Characters from './Characters';
+
+describe('Characters', () => {
+  it('matches a snapshot', () => {
+    const tree = renderer.create(
+      <Characters />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/services/rickAndMortyApi.js
@@ -0,0 +1,12 @@
+export const getCharacters = () => {
+  return fetch('https://cors-anywhere.herokuapp.com/http://rickandmortyapi.com/api/character', {
+    headers: {
+      origin: null
+    }
+  })
+    .then(res => res.json())
+    .then(json => ({
+      totalPages: json.info.pages,
+      results: json.results
+    }));
+};
