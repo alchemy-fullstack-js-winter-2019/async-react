6c113ce3119ed6e1716bfff33e2e5e4b6354368e
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-21 11:33:20 -0800

    extract pagination elements into Paging component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import './App.css';
 import Characters from './characters/Characters';
+import PropTypes from 'prop-types';
 
 export default class App extends Component {
   state = {
@@ -26,9 +27,14 @@ export default class App extends Component {
     return (
       < >
         <Header title={'Async React App'}/>
-        {currentPage < totalPages && <button id="next-btn" onClick={this.incrementCurrentPg}>Next</button>}
+        <Paging currentPage={currentPage}
+          totalPages={totalPages}
+          increment={this.incrementCurrentPg}
+          decrement={this.decrementCurrentPg}
+        />
+        {/* {currentPage < totalPages && <button id="next-btn" onClick={this.incrementCurrentPg}>Next</button>}
         <span>{currentPage} of {totalPages}</span>
-        {currentPage > 1 && <button id="prev-btn" onClick={this.decrementCurrentPg}>Previous</button>}
+        {currentPage > 1 && <button id="prev-btn" onClick={this.decrementCurrentPg}>Previous</button>} */}
         <Characters page={currentPage}
           updateTotalPages={this.updateTotalPages}
         />
@@ -36,3 +42,25 @@ export default class App extends Component {
     );
   }
 }
+
+function Paging({
+  currentPage,
+  totalPages,
+  increment,
+  decrement
+}) {
+  return (
+    < >
+    {currentPage < totalPages && <button id="next-btn" onClick={increment}>Next</button>}
+    <span>{currentPage} of {totalPages}</span>
+    {currentPage > 1 && <button id="prev-btn" onClick={decrement}>Previous</button>}
+    </>
+  );
+}
+
+Paging.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  increment: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired
+};
