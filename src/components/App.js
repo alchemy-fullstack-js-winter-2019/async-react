a838a34f08cad191be2517872a855f027fc00fe6
Kate Dameron <katerj33@gmail.com>
2019-02-21 12:27:16 -0800

    Updated App to just take the paging with characters component

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
-import Characters from './characters/Characters';
+import 'normalize.css';
+import CharactersWithPaging from './Paging';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-
-  state = {
-    currentPage: 1,
-    totalPages: 25
-  }
-
-  incrementPage = event => {
-    event.preventDefault();
-    const { currentPage, totalPages } = this.state;
-    this.setState({ currentPage: currentPage + 1 }, () => {
-      if(currentPage >= totalPages) {
-        console.log('hide forward button');
-      }
-    });
-  }
-
-  decrementPage = event => {
-    event.preventDefault();
-    const { currentPage } = this.state;
-    this.setState({ currentPage: currentPage - 1 });
-    if(currentPage <= 1) {
-      console.log('hide back button');
-    }
-  }
-
   render() {
     return (
-      <>
-      <div>
-        <button onClick={this.decrementPage}>&#60;</button>
-        <p>{`${this.state.currentPage} / ${this.state.totalPages}`}</p>
-        <button onClick={this.incrementPage}>&#62;</button>
-      </div>
-      <Characters />
-      </>
+      <CharactersWithPaging />
     );
   }
 }
