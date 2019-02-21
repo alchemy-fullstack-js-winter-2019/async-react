97ed0559be46f0e13015455410aaabc56bdbcd80
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-20 16:45:31 -0800

    Extract Characters into a service & Use service in Characters component complete; all tests passing

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
+jest.mock('../services/rickAndMortyApi.js');
+
 describe('App', () => {
-  it('matches a snapshot' , () => {
+  it('matches a snapshot', () => {
     const tree = renderer.create(
       <App />
     ).toJSON();
   
     expect(tree).toMatchSnapshot();
-  })
+  });
 });
