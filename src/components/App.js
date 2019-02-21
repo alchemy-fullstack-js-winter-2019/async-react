cf0c2f10a952b3a77bd0740812d45aed70c12635
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-20 16:47:24 -0800

    prev page button works

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,17 @@ export default class App extends Component {
     const { currentPage } = this.state;
     this.setState({ currentPage: currentPage + 1 });
   }
+  decreaseCount = () => {
+    const { currentPage } = this.state;
+    this.setState({ currentPage: currentPage - 1 });
+  }
   render() {
     const { currentPage, totalPages } = this.state;
     return (
       <>
         <h1>My list of things</h1>
         <button onClick={this.increaseCount}>Next Page</button>
-        <button>Prev Page</button>
+        <button onClick={this.decreaseCount}>Prev Page</button>
         <p>Current page: {currentPage}</p>
         <p>Total pages: {totalPages}</p>
         <Characters/>
