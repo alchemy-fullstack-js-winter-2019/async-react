270719220140a6fe5fec654f0a4aec7d6344cded
Aaron Dennis <AMDennis1987@gmail.com>
2019-02-21 09:20:31 -0800

    added buttons with page numbers, blocked by CORS

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-// import Characters from './characters/Characters';
+import Characters from './characters/Characters';
 
 class App extends Component {
 
+  state = {
+    currentPage: 1,
+    totalPages: 25
+  };
+
+  updatePageForward = () => {
+    const { currentPage, totalPages } = this.state;
+    if(currentPage > totalPages - 1) return;
+    this.setState({ currentPage: currentPage + 1 });
+  }
+
+  updatePageBackward = () => {
+    const { currentPage } = this.state;
+    if(currentPage < 2) return;
+    this.setState({ currentPage: currentPage - 1 });
+  }
+
+
+
   render() {
-    return <h1>My list of things</h1>;
+    const { currentPage, totalPages } = this.state;
+
+    return (
+      <>
+        <p>{currentPage} of {totalPages}</p>
+        <button onClick={this.updatePageBackward}>BACK</button>
+        <button onClick={this.updatePageForward}>NEXT</button>
+        <Characters />
+      </>
+    );
   }
 }
 
