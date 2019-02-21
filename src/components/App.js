d42f86e7716396ee99f43cf8bd26616090540fd4
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-20 17:03:53 -0800

    pages end at 25

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,35 @@ import Characters from './characters/Characters';
 
 
 export default class App extends Component {
+  state = {
+    currentPage: 1,
+    totalPages: 25
+  }
+
+  // this.setSate((state, props) => {
+  //   Return { title: state.title + ‘ you’ }
+  // })
+  
+
+  handleChange = () => {
+    const { currentPage, totalPages } = this.state;
+    if(currentPage < totalPages) {
+      this.setState({ currentPage: currentPage + 1 });
+
+    }
+  }
   
   render() {
+    const { currentPage, totalPages } = this.state;
     return (
       <>
         <Header />
+        <h4>{currentPage}/{totalPages}</h4>
+        
+        <button>BACK</button>
+        <button onClick={this.handleChange} >NEXT</button>
         <Characters />
       </>
     );
   }
-}
+};
