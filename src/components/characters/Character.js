e525e423dd46422ae5f4ad67aabdc42f21270f25
Kate Dameron <katerj33@gmail.com>
2019-02-21 12:14:06 -0800

    Character doin good - some style added

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../css/Character.css';
 
-function Character({ name, status, species, gender }) {
+function Character({ name, image, status, species, gender }) {
   return (
-    <tbody>
-      <tr>
-        <td>{name}</td>
-        <td>{species}</td>
-        <td>{status}</td>
-        <td>{gender}</td>
-      </tr>
-    </tbody>
+    <div className={styles.char}>
+      <h3>{name}</h3>
+      <img src={image}></img>
+      <p>{species} - {status} - {gender}</p>
+    </div>
   );
 }
 
 Character.propTypes = {
   name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
   species: PropTypes.string.isRequired,
   gender: PropTypes.string.isRequired,
