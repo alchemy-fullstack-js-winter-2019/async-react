49ec9652232a6cc535af18872a2a515a797097c3
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-21 10:03:00 -0800

    styling started on paging

--- src/components/paging/Paging.js
+++ src/components/paging/Paging.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Paging.css';
 
 function Paging({ totalPages, currentPage, increaseCount, decreaseCount }) {
   return (
-    <h5>
+    <h5 className={styles.Paging}>
       <button onClick={increaseCount}>Next</button>
       <span>Current Page:{currentPage}</span>
       <span>Total Pages:{totalPages}</span>
