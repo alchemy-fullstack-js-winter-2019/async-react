05accc360b91466044ad7d9611001d200abe7674
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-20 16:46:16 -0800

    can see current page on screen updates with button press

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import Characters from './characters/Characters';
 
 export default class App extends Component {
+  state = {
+    currentPage: 0,
+    totalPages: 0
+  }
+  increaseCount = () => {
+    const { currentPage } = this.state;
+    this.setState({ currentPage: currentPage + 1 });
+  }
   render() {
+    const { currentPage, totalPages } = this.state;
     return (
       <>
         <h1>My list of things</h1>
+        <button onClick={this.increaseCount}>Next Page</button>
+        <button>Prev Page</button>
+        <p>Current page: {currentPage}</p>
+        <p>Total pages: {totalPages}</p>
         <Characters/>
       </>
     );
