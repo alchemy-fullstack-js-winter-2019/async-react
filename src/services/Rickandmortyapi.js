3234e371fa379e9418f252edd7075d85731e60fb
cruzayye <johnnyc010392@gmail.com>
2019-02-22 09:19:31 -0800

    artist search displays

--- src/services/Rickandmortyapi.js
+++ src/services/Rickandmortyapi.js
@@ -10,3 +10,15 @@ export const getCharacters = page => {
       results: json.results
     }));
 };
+export const getLocations = page => {
+  return fetch(`https://rickandmortyapi.com/api/location?page=${page}`, {
+    headers: {
+      origin: null
+    }
+  })
+    .then(res => res.json())
+    .then(json => ({
+      totalPages: json.info.pages,
+      results: json.results
+    }));
+};
