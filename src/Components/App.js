1037edb4874c324dfb28486ae83934e2366137f9
Marcy Silverman <marcysilve225@gmail.com>
2019-02-21 21:16:31 -0800

    added more css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { CharactersWithPaging } from './characters/Characters';
 import { LocationsWithPaging } from './location/Locations';
+import styles from './css/Characters.css';
 
 
 export default class App extends Component {
   render() {
     return (
       <>
-      <CharactersWithPaging />
-      <LocationsWithPaging />
+      <CharactersWithPaging className={styles}/>
+      <LocationsWithPaging className={styles}/>
       </>
     );
   }
