2a6561da3debfcc8cf14299058aa7d4ac5e633c0
pickuppatterns <pickup.patterns@gmail.com>
2019-02-25 16:14:13 -0800

    characters test wrtitten & passing

--- src/components/characters/Characters.test.js
+++ src/components/characters/Characters.test.js
@@ -1,5 +1,13 @@
-describe ('character', () => {
-  it('matches a snapshot', () => {
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Characters from './Characters';
 
+describe ('characters', () => {
+  it('matches a snapshot', () => {
+    const tree = renderer.create(
+      <Characters characters={[{ name:'Ricky', gender:'male', species:'human', status:'alive' }]} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
