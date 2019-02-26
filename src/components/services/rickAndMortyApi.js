1b792ff566e55d1d8e46b85566723c57772c1294
pickuppatterns <pickup.patterns@gmail.com>
2019-02-25 16:30:17 -0800

    adds and api

---
+++ src/components/services/rickAndMortyApi.js
@@ -0,0 +1,14 @@
+
+export const getCharacters = () => {
+  return fetch ('https://rickandmortyapi.com/api/character', {
+    headers : {
+      origin: null
+    }
+
+  })
+    .then(res => res.json())
+    .then(json => ({
+      totalPages: json.info.pages, 
+      results: json.results
+    }));
+};
